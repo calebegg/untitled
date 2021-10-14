4296e8934f130877c7578b740b9566863f2ac260
Matthew <matthew@mindofmatthew.com>
2021-10-14 13:34:32 -0400

    Get rendering working

--- app/client/canvas/index.tsx
+++ app/client/canvas/index.tsx
@@ -1,34 +1,59 @@
-import { useState, useLayoutEffect, useCallback, useRef } from "react";
+import { useState, useLayoutEffect, useCallback } from "react";
 
 import { visuals } from "./style.module.css";
 
 export function ShaderCanvas() {
-  const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
-
-  const ref = useRef<HTMLCanvasElement | null>(null);
-
-  useLayoutEffect(() => {
-    console.log("layout...");
-    console.log(ref.current);
-  }, [ref.current]);
-
-  useLayoutEffect(() => {
-    if (ctx) {
-      ctx.fillStyle = "green";
-      ctx.fillRect(10, 10, 150, 100);
-    }
-  }, [ctx]);
+  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
 
   const callbackRef = useCallback(
     (canvas: HTMLCanvasElement | null) => {
       if (canvas) {
-        setCtx(canvas.getContext("2d"));
+        setCanvas(canvas);
       } else {
-        setCtx(null);
+        setCanvas(null);
       }
     },
-    [setCtx]
+    [setCanvas]
   );
 
-  return <canvas className={visuals} ref={ref} />;
+  const [[width, height], setDimensions] = useState<[number, number]>([0, 0]);
+
+  useLayoutEffect(() => {
+    if (canvas) {
+      canvas.width = canvas.clientWidth;
+      canvas.height = canvas.clientHeight;
+
+      setDimensions([canvas.clientWidth, canvas.clientHeight]);
+
+      const observer = new ResizeObserver(([{ contentBoxSize }]) => {
+        let [{ blockSize, inlineSize }] = contentBoxSize;
+        setDimensions([inlineSize, blockSize]);
+        console.log(`resize: ${inlineSize}, ${blockSize}`);
+      });
+
+      observer.observe(canvas);
+
+      return () => {
+        observer.disconnect();
+      };
+    } else {
+      setDimensions([0, 0]);
+    }
+  }, [canvas, setDimensions]);
+
+  useLayoutEffect(() => {
+    if (canvas && width > 0 && height > 0) {
+      const ctx = canvas.getContext("2d");
+
+      canvas.width = width;
+      canvas.height = height;
+
+      if (ctx) {
+        ctx.fillStyle = "white";
+        ctx.fillRect(50, 50, 200, 100);
+      }
+    }
+  }, [canvas, width, height]);
+
+  return <canvas className={visuals} ref={callbackRef} />;
 }
